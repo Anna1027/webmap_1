ae11f88e6c0b056eaa960dd629a1317c3d04b306
anna <annajoen@Annas-MacBook-Air.local>
2020-06-04 17:02:22 -0400

    implemented geolocate

---
+++ map.js
@@ -0,0 +1,58 @@
+'use strict'
+
+console.log('Loaded map.js')
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5uYTEwMjciLCJhIjoiY2tiMTU2c213MGdjZDJ6bXVhMGw0enF5aiJ9.PlYg3JKC4gxbYJWlqSvibw'
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-73.96024, 40.80877],
+    zoom: 12
+})
+
+// create an instance of NavigationControl
+let navigation = new mapboxgl.NavigationControl({
+    showCompass: false
+})
+
+// add the navigation to your map
+map.addControl(navigation, 'top-left')
+
+// create an instance of ScaleControl
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+})
+
+// add the scale to your map
+map.addControl(scale, 'bottom-right')
+
+//Geolocation 
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
+})
+map.addControl(geolocate, 'top-left')
+
+// this is an event handler
+geolocate.on('geolocate', function(event) {
+
+    // create new variables to store the attributes we're interested in from the event
+    let lng = event.coords.longitude
+    let lat = event.coords.latitude
+
+    // debug
+    console.log('geolocated:', lng, lat)
+
+    // format lng lat values and display them on our 'info' element
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
+
+
